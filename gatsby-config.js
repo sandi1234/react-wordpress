8ecfc990d440ad81a77437736830242fcea8d915
sandi1234 <sandi1234@users.noreply.github.com>
2019-12-11 23:23:03 +0100

    Rolled Back env
    Rolled Back env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,3 @@
-require("dotenv").config({
-  path: '.env',
-})
-
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -39,9 +35,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: process.env.API_URL,
+        baseUrl: 'seminside.com',
         // The protocol. This can be http or https.
-        protocol: process.env.API_PROTOCOL,
+        protocol: 'https',
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
